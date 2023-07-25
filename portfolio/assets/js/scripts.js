552c24d74340933964f2afc976c86571db644747
Moshiur Rahman Amit <moshiur.amit@gmail.com>
2023-07-25 16:29:55 +0600

    content added

--- portfolio/assets/js/scripts.js
+++ portfolio/assets/js/scripts.js
@@ -13,13 +13,9 @@
 // smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
-
         if (this.hash !== "") {
-
             event.preventDefault();
-
             var hash = this.hash;
-
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 700, function(){
